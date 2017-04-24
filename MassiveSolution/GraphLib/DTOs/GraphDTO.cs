e0969e64fdd3c20b30e950c49305d06e93194c0a
Sérgio Rocha <s.z.rocha@gmail.com>
2017-04-24 04:20:22 +0200

    Add DTOs and Graph class

---
+++ MassiveSolution/GraphLib/DTOs/GraphDTO.cs
@@ -0,0 +1,22 @@
+﻿using GraphLib.DTOs;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace GraphLoader
+{
+    [DataContract(Name = "Graph")]
+    public class GraphDTO
+    {
+        public GraphDTO(List<NodeDTO> nodes = null)
+        {
+            //empty graph
+            if (nodes == null)
+                Nodes = new List<NodeDTO>();
+
+            Nodes = nodes;
+        }
+
+        [DataMember]
+        public List<NodeDTO> Nodes;
+    }
+}
---
+++ MassiveSolution/GraphLib/DTOs/NodeDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace GraphLib.DTOs
+{
+    [DataContract(Name = "Node")]
+    [XmlRoot(ElementName = "node")]
+    public class NodeDTO
+    {
+        [DataMember(Name = "id", Order = 1)]
+        [XmlElement(ElementName = "id")]
+        public int NodeId { get; set; }
+
+        [DataMember(Name = "label", Order = 2)]
+        [XmlElement(ElementName = "label")]
+        public string Label { get; set; }
+
+        [DataMember(Name = "adjacentNodes", Order = 3)]
+        [XmlElement(ElementName = "adjacentNodes")]
+        public AdjacentNodesDTO AdjacentNodes { get; set; }
+    }
+}
