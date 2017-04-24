0d11f83628abcc8cb37d276d07f3147e9042c7f6
Sérgio Rocha <s.z.rocha@gmail.com>
2017-04-24 04:30:15 +0200

    New Folder Loader

---
+++ MassiveSolution/GraphLoader/FolderLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GraphLoader
+{
+    public class FolderLoader
+    {
+        private string _folder;
+
+        public FolderLoader(string folder)
+        {
+            if (string.IsNullOrWhiteSpace(folder))
+            {
+                throw new ArgumentException(nameof(folder));
+            }
+
+            if (!Directory.Exists(folder))
+            {
+                throw new DirectoryNotFoundException($"Could not find folder ('{Path.GetFullPath(folder)}')");
+            }
+
+            _folder = folder;
+        }
+
+        public IEnumerable<string> GetNodeFiles()
+        {
+            foreach (var file in Directory.EnumerateFiles(_folder, "*.xml"))
+            {
+                yield return File.ReadAllText(file);
+            }
+        }
+    }
+}
